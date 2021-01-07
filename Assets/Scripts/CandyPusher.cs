6de558fe802814bbbd17297821e449dc994d223e
Decoroader <secbittree@gmail.com>
2021-01-07 12:44:42 +0200

    Lab 3 - Player Control, 2, 3
    2.Create PlayerController and plan your code, 3.Basic movement from user input;
    adjusted for the mechanics of my project

---
+++ Assets/Scripts/CandyPusher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyPusher : MonoBehaviour
+{
+    private Rigidbody currentRigid;
+    private float speed = 3;
+
+    void Start()
+    {
+        currentRigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+		if (Input.GetMouseButtonUp(0))
+			currentRigid.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/CandyShifter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CandyShifter : MonoBehaviour
+{
+    public float coeffCoordinateMousToObject = 0.05f;
+    private float initMouseCoordinate;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            initMouseCoordinate = Input.mousePosition.x;
+
+        if (Input.GetMouseButton(0))
+		{
+            float shiftX = (initMouseCoordinate - Input.mousePosition.x) * coeffCoordinateMousToObject;
+            initMouseCoordinate = Input.mousePosition.x;
+            transform.position = new Vector3(transform.position.x - shiftX,
+                transform.position.y, transform.position.z);
+        }
+        
+    }
+}
