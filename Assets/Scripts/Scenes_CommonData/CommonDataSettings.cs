23aa5fda792f16bc476901613de831af80b0b70b
Decoroader <secbittree@gmail.com>
2021-03-01 18:21:38 +0200

    ScriptableObject to use started )

---
+++ Assets/Scripts/Scenes_CommonData/CommonDataSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "DataSettings")]
+public class CommonDataSettings : ScriptableObject
+{
+    public bool difficulty_mid;
+    public bool difficulty_hight;
+    public readonly int GAME = 1;
+    public readonly int MENU = 2;
+    public readonly int SORRY = 3;
+
+    public bool toGame = false;
+    public bool toMenu = false;
+    public bool sorry = false;
+}
