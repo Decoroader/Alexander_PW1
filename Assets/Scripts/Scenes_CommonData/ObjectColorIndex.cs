78c5002c4f7a6a6b36fe277cc6752721bed41b52
Decoroader <secbittree@gmail.com>
2021-03-27 11:20:56 +0200

    ScriptableObject for check candy color added

---
+++ Assets/Scripts/Scenes_CommonData/ObjectColorIndex.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "ObjectColorIndex")]
+public class ObjectColorIndex : ScriptableObject
+{
+	public static int GetCurrentObjectIndex(GameObject objectForIndex)
+	{
+		return (int)char.GetNumericValue(objectForIndex.name[objectForIndex.name.Length - 8]);
+	}
+}
