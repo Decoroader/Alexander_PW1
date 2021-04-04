567d751ec71f84be5c228497efdaeaa57276073a
Decoroader <secbittree@gmail.com>
2021-04-04 10:30:08 +0300

    frame for chosen difficulty added

--- Assets/Scripts/Scenes_CommonData/Menu.cs
+++ Assets/Scripts/Scenes_CommonData/Menu.cs
@@ -5,12 +5,21 @@ public class Menu : MonoBehaviour
     public AudioClip clickSound;
 
     public CommonDataSettings commonData;
+    
+    public GameObject buttonEasy;
+    public GameObject buttonMid;
+    public GameObject buttonHard;
+    public GameObject difficultyFrame;
 
     private AudioSource playerAudio;
+
+    private Vector3 frameDifficultyPosition;
 
     void Start()
     {
         playerAudio = GetComponent<AudioSource>();
+
+        FrameSetter(commonData.currentDifficulty);
     }
 
     public void FromMenuToGame()
@@ -21,20 +30,20 @@ public class Menu : MonoBehaviour
     public void LowDifficulty()
 	{
         playerAudio.PlayOneShot(clickSound, 1.0f);
-
         commonData.currentDifficulty = 1;
+        FrameSetter(commonData.currentDifficulty);
     }
     public void MidDifficulty()
 	{
         playerAudio.PlayOneShot(clickSound, 1.0f);
-
         commonData.currentDifficulty = 2;
+        FrameSetter(commonData.currentDifficulty);
     }
     public void HardDifficulty()
     {
         playerAudio.PlayOneShot(clickSound, 1.0f);
-
         commonData.currentDifficulty = 3;
+        FrameSetter(commonData.currentDifficulty);
     }
     public void BackGround()
     {
@@ -43,5 +52,21 @@ public class Menu : MonoBehaviour
     public void Voice()
     {
         commonData.sorry = true;
+    }
+    private void FrameSetter(int difficulty)
+	{
+        switch (difficulty)
+        {
+            case 1:
+                frameDifficultyPosition = buttonEasy.transform.position;
+                break;
+            case 2:
+                frameDifficultyPosition = buttonMid.transform.position;
+                break;
+            case 3:
+                frameDifficultyPosition = buttonHard.transform.position;
+                break;
+        }
+        difficultyFrame.transform.position = frameDifficultyPosition;
     }
 }
